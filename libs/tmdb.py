d8e5a11b67550c9e96db1a50291d20e0564d7784
Kyle Johnson <kyle.johnson@alumni.duke.edu>
2020-11-15 12:34:49 -1000

    normalize unicode for search

--- libs/tmdb.py
+++ libs/tmdb.py
@@ -20,6 +20,7 @@
 
 from __future__ import absolute_import, unicode_literals
 
+import unicodedata
 from math import floor
 from pprint import pformat
 from . import cache, data_utils, api_utils, settings, imdbratings, traktratings
@@ -72,7 +73,8 @@ def search_show(title, year=None):
     else:
         logger.debug('using title of %s to find show' % title)
         search_url = SEARCH_URL
-        params['query'] = fix_unicode(title)
+#        params['query'] = fix_unicode(title)
+        params['query'] = fix_unicode(unicodedata.normalize('NFKC', title))
         if year:
             params['first_air_date_year'] = str(year)
     resp = api_utils.load_info(search_url, params=params, verboselog=settings.VERBOSELOG)
