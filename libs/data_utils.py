1f1701485b374eb3dd9ec584c761f039bcc782e4
Kyle Johnson <kyle.johnson@alumni.duke.edu>
2021-11-27 08:47:43 -0600

    revert changes to MPAA ratings to move into another branch

--- libs/data_utils.py
+++ libs/data_utils.py
@@ -267,21 +267,18 @@ def add_main_show_info(list_item, show_info, full_info=True):
         content_ratings = show_info.get(
             'content_ratings', {}).get('results', {})
         if content_ratings:
+            mpaa = ''
             mpaa_backup = ''
-            video['Mpaa'] = ''
             for content_rating in content_ratings:
-                iso = content_rating.get('iso_3166_1', '')
-                c_rating = content_rating.get('rating', '')
-                if c_rating and iso:
-                    mpaa_raw = '%s %s' % (
-                        settings.ISOALPHAMAP.get(iso.upper(), ''), c_rating)
-                    if iso.lower() == settings.CERT_COUNTRY.lower():
-                        video['Mpaa'] = mpaa_raw
-                        break
-                    elif iso.lower() == 'us':
-                        mpaa_backup = mpaa_raw
-            if not video['Mpaa']:
-                video['Mpaa'] = mpaa_backup
+                iso = content_rating.get('iso_3166_1', '').lower()
+                if iso == 'us':
+                    mpaa_backup = content_rating.get('rating')
+                if iso == settings.CERT_COUNTRY.lower():
+                    mpaa = content_rating.get('rating', '')
+            if not mpaa:
+                mpaa = mpaa_backup
+            if mpaa:
+                video['Mpaa'] = settings.CERT_PREFIX + mpaa
         video['credits'] = video['writer'] = _get_credits(show_info)
         if settings.ENABTRAILER:
             trailer = _parse_trailer(show_info.get(
