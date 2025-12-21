d5c915872107d9e7abe2cd49e4537899da66e06e
Kyle Johnson <kyle.johnson@alumni.duke.edu>
2025-12-21 07:29:49 -0500

    removed unneeded debug logging

--- libs/data_utils.py
+++ libs/data_utils.py
@@ -250,7 +250,6 @@ def add_main_show_info(list_item, show_info, full_info=True):
         original_language = show_info['spoken_languages'][0]['iso_639_1']
     except KeyError:
         original_language = show_info.get('original_language', '')
-    logger.debug('using %s as the original language' % original_language)
     plot = _clean_plot(safe_get(show_info, 'overview', ''))
     vtag.setTitle(showname)
     vtag.setOriginalTitle(original_name)
